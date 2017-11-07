5a1a11ff53dd7faecb1bf6503cdc8e03dc8cbf9b
Luca Tironi <luca.tironi@gmail.com>
2017-11-07 23:53:06 +0100

    Add customers resource (model and controller)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resource :authentications, only: %i[create]
+  resources :customers, only: %i[index show create update destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105220206) do
+ActiveRecord::Schema.define(version: 20171107224455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
