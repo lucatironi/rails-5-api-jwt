3017528232a073ed4691e9caf0780fdfea51192e
Luca Tironi <luca.tironi@gmail.com>
2017-11-11 20:39:55 +0100

    Add seeds for customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+  { full_name: 'John Doe',   email: 'john.doe@example.com',   phone: '033 1234 5678' },
+  { full_name: 'Mark Smith', email: 'mark.smith@example.com', phone: '034 6789 1234' },
+  { full_name: 'Tom Clark',  email: 'tom.clark@example.com',  phone: '033 4321 9876' },
+  { full_name: 'Sue Palmer', email: 'sue.palmer@example.com', phone: '034 9876 1234' },
+  { full_name: 'Kate Lee',   email: 'kate.lee@example.com',   phone: '033 6789 4321' }
+].each do |customer_attributes|
+  Customer.create(customer_attributes)
+end
+
+User.create(email: 'user@example.com', password: 'password')
